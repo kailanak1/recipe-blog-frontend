bfdf08944ec77fff8e43e125b36f43076aac5eaa
kailana <kailanakahawaii@gmail.com>
2020-04-26 20:16:35 -0700

    can add ingredient name

--- src/components/AddRecipeForm.js
+++ src/components/AddRecipeForm.js
@@ -2,17 +2,22 @@ import React from 'react'
 //need to send add recipe down from app, api call to backend
 
 class AddRecipeForm extends React.Component{
-    state = {
-        title:"",
-        ingredients: [],
-        steps: [],
-        tags: []
-    }
+   
+            state = {
+                title:"",
+                ingredients: [
+                    {name:"", amount:""}
+                ],
+                steps: [],
+                tags: []
+            }
+  
 
-    addIngredient(e){
+    addIngredient = (e) => {
         e.preventDefault()
-        this.setState({ingredients: [...this.state.ingredients, ""]
-        })
+        this.setState((prevState=> ({
+            ingredients: [...prevState.ingredients, {name:"", amount:""}],
+        })))
     }
 
     addSteps(e){
@@ -26,34 +31,60 @@ class AddRecipeForm extends React.Component{
     //     this.setState({ingredients: this.state.ingredients})
     // }
 
-    handleChange = (e, index) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        
-        })   
+    handleChange = (e) => {
+        if (["name", "amount"].includes(e.target.className)){
+            let ingredients = [...this.state.ingredients]
+            ingredients[e.target.dataset.id][e.target.className] = e.target.value
+            this.setState({[e.target.name]: e.target.value})
+        } else {
+            this.setState({ [e.target.name]: e.target.value})
+        }
     }
+
+
+    // handleIngredientChange = (event, index) => {
+    //     const newState = {...this.state.ingredients, [event.target.name]: event.target.value}
+    //     this.setState({
+    //         ...this.state,
+    //         ingredients: newState
+    //     })
+    // }
+
+  
 
    
 
 
     render(){
       console.log(this.state)
+      let {title, ingredients, steps, tags} = this.state
+      
         return (
             <div>
             <h1>Add a Recipe!</h1>
-            <form id="recipe-form"> 
+            <form id="recipe-form" onChange={this.handleChange}> 
                  <label>Title</label>
                  <br></br>
-                 <input placeholder="Title"></input>
+                 <input placeholder="Title" onChange={ e => this.handleChange(e)} name="title" value={title}></input>
                  <br></br>
                 <label>Ingredients</label>
                 {
                     this.state.ingredients.map((ingredient, index) => {
+                        let ingredientId = `ingredient-${index}`, amountId= `amount-${index}`
                         return( 
                             <div key={index}>
-                                <input placeholder="Ingredient Name" onChange={(e, index) => this.handleChange(e)} name="ingredient name"/>
+                                <label htmlFor={ingredientId}>{`Ingredient #${index + 1}`}</label>
+                                <input placeholder="Ingredient Name" 
+                                type="text"
+                                name={ingredientId}
+                                data-id={index}
+                                id={ingredientId}
+                                value={ingredients[index].name}
+                                className="name"
+                                onChange={(e)=>this.handleChange}
+                            />
 
-                                {/* <input placeholder="Ingredient Amount" onChange={(e, index) => this.handleChange(e)} value={this.state.ingredient.amount}/> */}
+                                {/* <input placeholder="Ingredient Amount" onIngrChange={(e, index) => this.handleChange(e)} value={this.state.ingredients.amount}/> */}
 
                                 {/* <button onClick={(e)=> this.removeIngredient(e)}>Remove</button> */}
                                
@@ -66,13 +97,25 @@ class AddRecipeForm extends React.Component{
                 <br></br>
               
                 <label>Steps</label>
+                {
+                    this.state.steps.map((step, index) => {
+                        return( 
+                            <div key={index}>
+                                <textarea placeholder="Step" onChange={(e, index) => this.handleChange(e, index)} name="steps" />
+                               
+                            </div>
+                        )
+                    })
+                }
                 <br></br>
-                <button onClick={(e)=> this.addSteps(e)}>Add Steps</button>
+                <button onClick={(e)=> this.addSteps(e)}>Add Step</button>
                 <br></br>
 
                 <label>Tags</label>
                 <br></br>
-                
+                <button onClick={(e)=> this.addTag(e)}>Add Tags</button>
+                <br></br>
+                <br></br>
                 <button>Publish Recipe</button>
             </form>
             </div>
--- src/services/api.js
+++ src/services/api.js
@@ -44,6 +44,8 @@ const login = data => {
         })
     }
 
+    
+
   export const api = {
       auth: {
           login, 
