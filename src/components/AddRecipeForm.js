e0c6a4635a99c22ab2c53ee8bd9c74a6413ed6dd
kailana <kailanakahawaii@gmail.com>
2020-05-01 10:39:13 -0700

    hooking up form

--- src/components/AddRecipeForm.js
+++ src/components/AddRecipeForm.js
@@ -1,18 +1,18 @@
-import React, {useReducer} from 'react'
-import { render } from '@testing-library/react'
+import React, {useReducer, useState} from 'react'
+
+import { useForm, useFieldArray } from "react-hook-form";
+
 
 const maxChars = 80
 const smallertextareastyle= {
     padding: "9px", 
     boxSizing: "border-box", 
     fontSize: "15px", 
-    minHeight: "100px",
-    minWidth: "300px"
+    minHeight: "75px",
+    minWidth: "100px"
   } 
-
-
 const AddRecipeForm  = (props) => {
-   
+
 
     const [recipe, setRecipe] = useReducer(
         (recipe, newRecipe) => ({...recipe, ...newRecipe}),
@@ -23,43 +23,116 @@ const AddRecipeForm  = (props) => {
         ingredients: [
             {name: "", amount: ""}
         ],
-            steps: [],
-            tags: [],
-            charsLeft: maxChars
+        steps: [],
+        tags: [],
+        charsLeft: maxChars
     }
-
     )
-
-         
+   
     const handleChange = (e) =>{
         const {name, value} = e.target
         setRecipe({[name]: value})
     }
+    
+    const addStepToRecipe = (e) => {
+        let newStep = ""
+        setRecipe({ 
+            steps: [...recipe.steps, newStep]
+        })
+    }
 
-    const handleClick = () => {
+    // let totalSteps = 0;
+
+
+
+    // const handleAddStep = (e) => {
+    //     console.log(e.target.name)
+    //     e.preventDefault()
+    //     setRecipe({
+    //         ...recipe,
+    //         steps: [
+    //             ...recipe.steps, { [e.target.name]:e.target.value }
+    //         ]
+    //     })
+    // }
+    // const stepsMapper = () => {
+    //     {
+    //     return recipe.steps.map((step, index) =>{
+    //         const stepNumber = index+1
+    //         return( 
+    //         <div key={index}>
+    //             <textarea
+    //                 name={stepNumber}
+    //                 placeholder={stepNumber}
+    //                 value={recipe.steps[stepNumber]}
+    //                 onChange={handleChange}
+    //             />
+    //         </div>
+    //         )
+    //     })
+    //     }
+    // }
+
+    const handleRemoveStep = () => {
 
     }
 
 
-          
-    return(
+    const stepsMapper = () => {
+        
+        return (
         <div>
-            <form>
-                <label>Title</label>
-                <br></br>
-                <input type="text" onChange={handleChange} value={recipe.title} name="title"></input>
-                <br></br>
-                <label>Steps</label>
-                <br></br>
-                <textarea onChange={handleChange} value={recipe.steps} name="steps"></textarea>
-                <button onClick={handleClick}>Add a new Step</button>
-                {/* <textarea onChange={handleChange}  value={recipe.steps.step2} name="step2"></textarea> */}
-            </form>
-        </div>
-    )
+        {fields.map((item, index) => (
+          <div key={item.id}>
+            <input
+            name={recipe.steps} 
+            onChange={handleChange} 
+            style={smallertextareastyle} 
+            defaultValue={`Step ${index +1} `} 
+            ref={register()} />
+            <button onClick={() => remove(index)}>Delete</button>
+          </div>
+        ))}
+      <section>
+        <button type="button" onClick={() => append({ name: "step" })} >
+          Add step
+        </button>
+      </section>
+      </div>
+        )
+    }
 
+
+
+
+  const { register, control, handleSubmit } = useForm({
+    // defaultValues: {}; you can populate the fields by this attribute 
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "test"
+  });
+  
+  return (
+      <div>
+          <h1>Add a new recipe!</h1>
+    <form onSubmit={handleSubmit(data => console.log("data", data))}>
+        <label>Title</label>
+        <br></br>
+        <input onChange={handleChange} name="title" placeholder="Title" ref={register}></input>
+        <br></br>
+        <label>Steps</label>
+      {stepsMapper()}
+      <input type="submit"></input>
+    </form>
+    </div>
+  );
 
 
 }
+
+
+
+    
 
 export default AddRecipeForm 
