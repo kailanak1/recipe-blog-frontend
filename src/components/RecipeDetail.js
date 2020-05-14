090abe96e485fd080ad42c381fbb61240ca468ac
kailana <kailanakahawaii@gmail.com>
2020-05-14 16:03:03 -0700

    recdetail renders w dynamic routing

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -9,11 +9,12 @@ const RecipeDetail = props => {
 const getRecipe = () => {
     const id = props.match.params.id
     const allRecs = props.appState.recipes
-    console.log(allRecs)
-    for (let recipes of allRecs){
-        if(recipes.id == id){
-            console.log(recipes)
-        }
+    for (let recipe of allRecs){
+        if(recipe.id == id){
+           return recipe
+        
+    }
+    
     }
 }
 
@@ -24,48 +25,65 @@ const handleClick = () =>  {
 
 
 
-// const ingredientsMapper = () => {
-//     let ingredients = props.recipe.ingredients
-//     if(ingredients){
-//         return ingredients.map((ingredient, index) => {
-//             return <ListGroup>
-//                 <ListGroup.Item key={index}>{`${ingredient.amount} ${ingredient.name}`}</ListGroup.Item>
-//             </ListGroup>
-//         })
-//     }
-// }
+const ingredientsMapper = () => {
+    let recipe = getRecipe()
+    let ingredients = recipe.ingredients
+    if(ingredients){
+        return ingredients.map((ingredient, index) => {
+            return <ListGroup>
+                <ListGroup.Item key={index}>{`${ingredient.amount} ${ingredient.name}`}</ListGroup.Item>
+            </ListGroup>
+        })
+    }
+}
 
-// const stepsMapper = () => {
-//     let steps = props.recipe.steps
-//     if(steps){
-//         return steps.map((step, index) => {
-//             return <ListGroup>
-//                 <ListGroup.Item key={index}>{`Step ${index+1}. ${step.step_summary}`}</ListGroup.Item>
-//             </ListGroup>
-//         })
-//     } else {
-//         return <ListGroup>
-//                 <ListGroup.Item>No Steps</ListGroup.Item>
-//             </ListGroup>
-//     }
-// }
+const stepsMapper = () => {
+    let recipe = getRecipe()
+    let steps = recipe.steps
+    if(steps){
+        return steps.map((step, index) => {
+            return <ListGroup>
+                <ListGroup.Item key={index}>{`Step ${index+1}. ${step.step_summary}`}</ListGroup.Item>
+            </ListGroup>
+        })
+    } else {
+        return <ListGroup>
+                <ListGroup.Item>No Steps</ListGroup.Item>
+            </ListGroup>
+    }
+}
 
-// const tagsMapper = () => {
-//     let tags = props.recipe.tags
-//     if(tags){
-//         return tags.map((tag)=> {
-//             return `${tag.name} `
-//         })
-//     }
-// }
+const tagsMapper = () => {
+    let recipe = getRecipe()
+    let tags = recipe.tags
+    if(tags){
+        return tags.map((tag)=> {
+            return `${tag.name} `
+        })
+    }
+}
 
-// const makeBig = () => {
+const makeBig = () => {
     
-// }
+}
 
-// const favoriteRecipe = (recipeId) => {
-//     props.onFavoriteRecipe(recipeId)
-// }
+const favoriteRecipe = (recipeId) => {
+    props.onFavoriteRecipe(recipeId)
+}
+
+const title = () => {
+    let recipe = getRecipe()
+    return recipe.title
+}
+
+const summary = () => {
+    let recipe = getRecipe()
+    if (recipe.summary.length){
+    return recipe.summary
+    } else{
+        return "No summary given"
+    }
+}
 
 
 return(
@@ -73,35 +91,35 @@ return(
 
     <div>
 
-        {getRecipe()}
 
-    {/* <Card style={{width: '36rem'}}>
+
+     <Card style={{width: '36rem'}}>
         <div className="btn-lef" style={{display: 'flex'}}>
         <Button variant="primary" onClick = {handleClick} style={{ marginRight: "auto" }}>Go Back</Button>
        <Button variant="primary" onClick = {makeBig} style={{ marginLeft: "auto" }}>Increase Font Size</Button> 
         </div>
-    <div key={props.recipe.id}>
-        <Card.Title style={{fontSize:'50px' }}>{props.recipe.title}</Card.Title>
-        <Card.Text>{props.recipe.summary ? <div>{props.recipe.summary}</div> : "No summary given"}</Card.Text>
+    <div key={props.match.params.id}>
+        <Card.Title style={{fontSize:'50px' }}>{title()}</Card.Title>
+        <Card.Text><div>{summary()}</div></Card.Text>
 
         <br></br>
 
-        {props.recipe.ingredients ? <div>{ingredientsMapper()}</div> : "No ingredients written"}
+        <div>{ingredientsMapper()}</div> 
         <br></br>
        
-        {props.recipe.steps ? <div>{stepsMapper()}</div> : "No steps given"}
+        <div>{stepsMapper()}</div>
         <br></br>
    
 
-     <small>Tags: {props.recipe.tags ?  <div>{tagsMapper()}</div> : "No tags"  }</small> 
-         <Button onClick={() => favoriteRecipe(props.recipe.id)}>Favorite</Button> 
+     <small>Tags: <div>{tagsMapper()}</div></small> 
+         <Button onClick={() => favoriteRecipe(props.match.params.id)}>Favorite</Button> 
         <br></br>
         <br></br>
 
         <br></br>
         
     </div>
-    </Card>  */}
+    </Card>  
     </div>
 )
 
